cced3f05ecf26c5383fa9a7ed8d88ca5d71f5407
shtirlic <serg@podtynnyi.com>
2012-11-09 01:50:37 +0400

    Test cleanups

--- spec/jsonp_spec.rb
+++ spec/jsonp_spec.rb
@@ -1,5 +1,5 @@
 require 'backports'
-require_relative 'spec_helper'
+require 'spec_helper'
 
 describe Sinatra::Jsonp do
   before do
@@ -17,19 +17,19 @@ describe Sinatra::Jsonp do
     end
   end
 
-  it "should return JSON if no callback passed" do
+  it "returns JSON if no callback passed" do
     get '/method'
     body.should == '["hello","hi","hallo"]'
   end
-  it "should return JSONP if callback passed via GET param" do
+  it "returns JSONP if callback passed via request params" do
     get '/method?callback=functionA'
     body.should == 'functionA(["hello","hi","hallo"])'
   end
-  it "should return JSONP if callback passed via method param" do
+  it "returns JSONP if callback passed via method param" do
     get '/method_with_params'
     body.should == 'functionA(["hello","hi","hallo"])'
   end
-  it "should return JSONP with callback passed via method params even if it passed via GET param" do
+  it "returns JSONP with callback passed via method params even if it passed via request param" do
     get '/method_with_params?callback=functionB'
     body.should == 'functionA(["hello","hi","hallo"])'
   end
