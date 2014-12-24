5e2776c6a2eb51a56c145aa308108f9b2450a1cd
Darren Coxall <darren@darrencoxall.com>
2014-12-24 11:17:28 +0000

    Move final response logic into new method

--- lib/sinatra/jsonp.rb
+++ lib/sinatra/jsonp.rb
@@ -7,27 +7,21 @@ module Sinatra
 
     def jsonp(*args)
       return if args.size < 1
-
       data = MultiJson.dump(args[0], :pretty => display_pretty_json?)
       callback = extract_callback_name(args[1])
-
-      # If we have a callback perform some basic sanitization and set the
-      # response content type and the eventual response body
-      if callback
-        callback.tr!('^a-zA-Z0-9_$\.', '')
-        content_type :js
-        response = "#{callback}(#{data})"
-      else
-        # If no callback then set the response content type to json and return
-        # the serialized data
-        content_type :json
-        response = data
-      end
+      type, response = determine_response(data, callback)
+      content_type(type)
       response
     end
     alias JSONP jsonp
 
     private
+
+    def determine_response(data, callback = nil)
+      return [:json, data] if callback.nil?
+      callback.tr!('^a-zA-Z0-9_$\.', '')
+      [:js, format("%s(%s)", callback, data)]
+    end
 
     def display_pretty_json?
       !!(settings.respond_to?(:json_pretty) && settings.json_pretty)
