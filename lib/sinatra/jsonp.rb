898fae9b8ccc0f69374fa29bec1fc78eeca96b1c
Darren Coxall <darren@darrencoxall.com>
2014-12-24 10:46:08 +0000

    Introduce guard condition and extract setting checks

--- lib/sinatra/jsonp.rb
+++ lib/sinatra/jsonp.rb
@@ -4,27 +4,41 @@ require 'multi_json'
 module Sinatra
   module Jsonp
     def jsonp(*args)
-      if args.size > 0
-        data = MultiJson.dump args[0], :pretty => settings.respond_to?(:json_pretty) && settings.json_pretty
-        if args.size > 1
-          callback = args[1].to_s
-        else
-          ['callback','jscallback','jsonp','jsoncallback'].each do |x|
-            callback = params.delete(x) unless callback
-          end
+      # requires 1 or more arguments
+      return if args.size < 1
+
+      # The first argument is the object to serialize
+      data = MultiJson.dump(args[0], :pretty => display_pretty_json?)
+      # If we have another argument it is the callback function name
+      if args.size > 1
+        callback = args[1].to_s
+      else
+        # If not then determine the callback based on the following parameters
+        ['callback','jscallback','jsonp','jsoncallback'].each do |x|
+          callback = params.delete(x) unless callback
         end
-        if callback
-          callback.tr!('^a-zA-Z0-9_$\.', '')
-          content_type :js
-          response = "#{callback}(#{data})"
-        else
-          content_type :json
-          response = data
-        end
-        response
       end
+      # If we have a callback perform some basic sanitization and set the
+      # response content type and the eventual response body
+      if callback
+        callback.tr!('^a-zA-Z0-9_$\.', '')
+        content_type :js
+        response = "#{callback}(#{data})"
+      else
+        # If no callback then set the response content type to json and return
+        # the serialized data
+        content_type :json
+        response = data
+      end
+      response
     end
     alias JSONP jsonp
+
+    private
+
+    def display_pretty_json?
+      !!(settings.respond_to?(:json_pretty) && settings.json_pretty)
+    end
   end
   helpers Jsonp
 end
