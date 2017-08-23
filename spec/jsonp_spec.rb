23f483f2078a80ff17fc3dc3ec562cbda7d51def
SAKATA Sinji <sinji@sakasin.net>
2017-08-24 00:11:56 +0900

    Change to `expect` syntax
    `should` is deprecated.

--- spec/jsonp_spec.rb
+++ spec/jsonp_spec.rb
@@ -19,25 +19,25 @@ describe Sinatra::Jsonp do
 
   it "returns JSON if no callback passed" do
     get '/method'
-    body.should == '["hello","hi","hallo"]'
+    expect(body).to eq '["hello","hi","hallo"]'
   end
   it "returns JSONP if callback passed via request params" do
     get '/method?callback=functionA'
-    body.should == 'functionA(["hello","hi","hallo"])'
+    expect(body).to eq 'functionA(["hello","hi","hallo"])'
   end
 
   it "returns JSONP with sanitized callback" do
     get '/method', { :callback=>'foo<script>alert(1)</script>' }
-    body.should == 'fooscriptalert1script(["hello","hi","hallo"])'
+    expect(body).to eq 'fooscriptalert1script(["hello","hi","hallo"])'
   end
 
   it "returns JSONP if callback passed via method param" do
     get '/method_with_params'
-    body.should == 'functionA(["hello","hi","hallo"])'
+    expect(body).to eq 'functionA(["hello","hi","hallo"])'
   end
   it "returns JSONP with callback passed via method params even if it passed via request param" do
     get '/method_with_params?callback=functionB'
-    body.should == 'functionA(["hello","hi","hallo"])'
+    expect(body).to eq 'functionA(["hello","hi","hallo"])'
   end
 
   it "return pretty JSON if :json_pretty enabled" do
@@ -50,6 +50,6 @@ describe Sinatra::Jsonp do
       end
     end
     get '/method'
-    body.should == "[\n  \"hello\",\n  \"hi\",\n  \"hallo\"\n]"
+    expect(body).to eq "[\n  \"hello\",\n  \"hi\",\n  \"hallo\"\n]"
   end
 end
