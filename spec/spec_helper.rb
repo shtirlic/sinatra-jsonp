cc8c4d7cd9b8bac8c8c023ce832711d1c9b418df
SAKATA Sinji <sinji@sakasin.net>
2017-08-23 23:51:08 +0900

    Update rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'sinatra/contrib'
 require 'sinatra/jsonp'
 
 RSpec.configure do |config|
-  config.expect_with :rspec, :stdlib
+  config.expect_with :rspec
   config.include Sinatra::TestHelpers
 end
 
